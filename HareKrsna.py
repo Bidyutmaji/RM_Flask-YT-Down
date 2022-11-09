29b75d5747f23b94c87b9b8fd04f2c052d629fd0
Bidyut Maji <bidyutmaji12345@gmail.com>
2022-11-09 11:43:27 +0530

    RM initial commit

--- HareKrsna.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect(database="dic_live",
-                        host="dim-database.cizy3y0o3xp1.ap-south-1.rds.amazonaws.com",
-                        user="postgresuser",
-                        password="rT6yb%ffgTTvvhf67",
-                        port="5432")
-
-cursor = conn.cursor()
